1059f7ed0f0488277bd29581f63aa2177d7ee754
Aisha Rahmadia Aqilah <63826055+aishaaaara@users.noreply.github.com>
2021-03-24 09:43:06 +0700

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import axios from 'axios'
-import Navbarr from '../components/navbar'
+import axios from "axios";
+import NavBar from "../components/navbar";
 
 class Home extends React.Component{
     constructor(){
@@ -8,13 +8,15 @@ class Home extends React.Component{
         this.state = {
             token: "",
             userName: null,
-            pegawaiCount: 0
+            pegawaiCount: 0,
+            pelanggaranCount: 0
         }
         if (localStorage.getItem("token")) {
             this.state.token = localStorage.getItem("token")
         } else {
             window.location = "/login"
         }
+        this.headerConfig.bind(this) 
     }
     headerConfig = () => {
         let header = {
@@ -38,37 +40,92 @@ class Home extends React.Component{
           console.log(error);
         });
     }
+    getSiswa = () => {
+        let url = "http://localhost:2000/siswa";
+        // mengakses api untuk mengambil data pegawai
+        console.log(this.headerConfig())
+        axios.get(url, this.headerConfig())
+        .then(response => {
+          // mengisikan data dari respon API ke array pegawai
+          this.setState({siswaCount: response.data.count});
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+    getJurusan = () => {
+        let url = "http://localhost:2000/jurusan";
+        // mengakses api untuk mengambil data pegawai
+        console.log(this.headerConfig())
+        axios.get(url, this.headerConfig())
+        .then(response => {
+          // mengisikan data dari respon API ke array pegawai
+          this.setState({jurusanCount: response.data.count});
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+    
     componentDidMount(){
         this.getUser()
         this.getPegawai()
+        this.getSiswa()
+        this.getJurusan()
     }
-
     render(){
         return(
             <div>
-            <Navbarr />
-            <div className="container mt-2">
-                <h3 className="my-2">
-                    <strong>Welcome back, {this.state.userName}</strong>
-                </h3>
-                <div className="row">
-                    {/* pegawai count */}
-                    <div className="col-lg-4 col-md-6 col-sm-12 mt-2">
-                        <div className="card">
-                            <div className="card-body bg-success">
-                                <h4 className="text-dark">
-                                    <strong>Jumlah Pegawai</strong>
-                                </h4>
-                                <h1 className="text-white">
-                                    <strong>{this.state.pegawaiCount}</strong>
-                                </h1>
+                <NavBar/>
+                <div className="container mt-2">
+                    <h3 className="my-2">
+                        <strong>Welcome back, {this.state.userName}</strong>
+                    </h3>
+                    <div className="row">
+                        {/* pegawai count */}
+                        <div className="col-lg-4 col-md-6 col-sm-12 mt-2">
+                            <div className="card">
+                                <div className="card-body bg-success">
+                                    <h4 className="text-dark">
+                                        <strong>Jumlah Pegawai</strong>
+                                    </h4>
+                                    <h1 className="text-white">
+                                        <strong>{this.state.pegawaiCount}</strong>
+                                    </h1>
+                                </div>
+                            </div>
+                        </div>
+                        {/* siswa count */}
+                        <div className="col-lg-4 col-md-6 col-sm-12 mt-2">
+                            <div className="card">
+                                <div className="card-body bg-warning">
+                                    <h4 className="text-dark">
+                                        <strong>Jumlah Siswa</strong>
+                                    </h4>
+                                    <h1 className="text-white">
+                                        <strong>{this.state.siswaCount}</strong>
+                                    </h1>
+                                </div>
+                            </div>
+                        </div>
+                        {/* jurusan count */}
+                        <div className="col-lg-4 col-md-6 col-sm-12 mt-2">
+                            <div className="card">
+                                <div className="card-body bg-danger">
+                                    <h4 className="text-dark">
+                                        <strong>Jumlah Jurusan</strong>
+                                    </h4>
+                                    <h1 className="text-white">
+                                        <strong>{this.state.jurusanCount}</strong>
+                                    </h1>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )
-        }
+        )
     }
+}
+
 export default Home
